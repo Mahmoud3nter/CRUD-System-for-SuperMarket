9abab1b1dd436ef4c144fcc5d2b71525d1436505
Mahmoud_Anter <150552080+Mahmoud3nter@users.noreply.github.com>
2024-03-25 11:32:46 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,32 @@
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit = document.getElementById('submit');
+let mood = 'creat';
+let temp;
+//get tot 
+function gettotal(){
+    
+
+}
+
+
+//clear input
+
+
+
+
+//read
+
+//delet
+
+
+//deletall
+
+
+//update
